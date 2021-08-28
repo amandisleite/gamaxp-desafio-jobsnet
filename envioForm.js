18ffb077ac107c7efbbdd986809fce2e3abbca4a
Amanda Leite <mandirleite@gmail.com>
2021-08-28 12:05:13 -0300

    envioForm.js ajustado e dados do formulário conectados ao banco de dados

--- envioForm.js
+++ envioForm.js
@@ -1,55 +1,35 @@
-function Candidate(event) {
-    
-    const createCandidate = async (candidato) => {
-        
-         let   nomeCompleto = document.getElementById('inputNome').value;
-         let   cpf = document.getElementById('inputCpf').value;
-         let   dataDia = document.getElementById('dataDia').value;
-         let   dataMes = document.getElementById('dataMes').value;
-         let   dataAno = document.getElementById('dataAno').value;
-         let   estadoCivil = document.getElementById('inputEstadoCivil').value;
-         let   sexo = document.getElementById('inputSexo').value;
-         let   cep = document.getElementById('inputCep').value;
-         let   endereco = document.getElementById('inputEndereco').value;
-         let   numero = document.getElementById('inputNumero').value;
-         let   complemento = document.getElementById('inputComplemento').value;
-         let   bairro = document.getElementById('inputBairro').value;
-         let   cidade = document.getElementById('inputCidade').value;
-         let   telefone = document.getElementById('inputTelefone').value;
-         let   email = document.getElementById('inputEmail').value;
-         let   profissao = document.getElementById('inputProfissao').value;
-         let   cargo = document.getElementById('inputCargo').value;
-         let   veiculo = document.getElementById('inputVeiculo').value;
-         let   cnh = document.getElementById('inputCNH').value;
-
-
+const Form = () => {
 
         let form = {
             
-            nomeCompleto: `${nomeCompleto}`,
-            cpf: `${cpf}`,
-            dataDia: `${dataDia}`,
-            dataMes: `${dataMes}`,
-            dataAno: `${dataAno}`,
-            estadoCivil: `${estadoCivil}`,
-            sexo: `${sexo}`,
-            cep: `${cep}`,
-            endereco: `${endereco}`,
-            numero: `${numero}`,
-            complemento: `${complemento}`,
-            bairro: `${bairro}`,
-            cidade: `${cidade}`,
-            telefone: `${telefone}`,
-            email: `${email}`,
-            profissao: `${profissao}`,
-            cargo: `${cargo}`,
-            veiculo: `${veiculo}`,
-            cnh: `${cnh}`
-
+            nomeCompleto: document.getElementById('inputNome').value,
+            cpf: document.getElementById('inputCpf').value,
+            dataDia: document.getElementById('dataDia').value,
+            dataMes: document.getElementById('dataMes').value,
+            dataAno: document.getElementById('dataAno').value,
+            estadoCivil: document.getElementById('inputEstadoCivil').value,
+            sexo: document.getElementById('inputSexo').value,
+            cep: document.getElementById('inputCep').value,
+            endereco: document.getElementById('inputEndereco').value,
+            numero: document.getElementById('inputNumero').value,
+            complemento: document.getElementById('inputComplemento').value,
+            bairro: document.getElementById('inputBairro').value,
+            cidade: document.getElementById('inputCidade').value,
+            telefone: document.getElementById('inputTelefone').value,
+            email: document.getElementById('inputEmail').value,
+            profissao: document.getElementById('inputProfissao').value,
+            cargo: document.getElementById('inputCargo').value,
+            veiculo: document.getElementById('inputVeiculo').value,
+            cnh: document.getElementById('inputCNH').value,
         };
     
         console.log(form);
+        return form
+
+    }
     
+    const createCandidate = async (candidato) => {
+
         try {
             const user = fetch('http://localhost:5000/register', {
                 method: "POST",
@@ -57,21 +37,22 @@ function Candidate(event) {
                     'Accept': 'application/json',
                     'Content-Type':'application/json'
                 },
-                body: JSON.stringify(form)
+                body: JSON.stringify(Form())
             });
             if (user.status === 200) {
-                console.log(form);
+                console.log(Form());
                 alert('deu certo')
             }
         } catch (error) {
             alert ('deu errado');
-            console.log(form);
+            console.log(Form());
         }
+
     }
 
-}
+    
 
-// document.getElementById('submit').addEventListener('click', Candidate);
+
 
 
 
