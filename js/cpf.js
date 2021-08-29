fc4ed085614f31fbcbe01af4c9cb0384bc6cc988
Amanda Leite <mandirleite@gmail.com>
2021-08-29 13:32:51 -0300

    upando

---
+++ js/cpf.js
@@ -0,0 +1,65 @@
+console.log('javascript carregado')
+
+function validaCPF(cpf) {
+
+    if (cpf.length != 11) {
+        return false;
+    } else {
+        var numeros = cpf.substring(0, 9); // pegou os 9 primeiros caracteres
+        var digitos = cpf.substring(9); // pega os digitos finais
+
+        var soma = 0;
+        for (var i = 10; i > 1; i--) { // diminui nos números, vai pra baixo
+            soma += numeros.charAt(10 - i) * i; // charAt busca as posições até encontrar o que tá procurando
+            //Primeiramente multiplica-se os 9 primeiros dígitos pela sequência decrescente de números de 10 à 2 e soma os resultados.
+            // a soma dada é essa da frase de cima
+        }
+            var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+            // pega o resto da divisão da soma por 11; tem que ser menor que 2
+            // pergunta se é verdadedeira a condição e atribui 0 se isso concretiza
+            // senão for verdade, vai pra operação 11 - (soma % 11)
+
+            // validação do primeiro dígito
+            if (resultado != digitos.charAt(0)) { //se o resultado for diferente do primeiro digito (o da posição 0)
+                return false;
+            }
+
+            soma = 0;
+            numeros = cpf.substring(0, 10);
+
+            for (var k = 11; k > 1; k--) {
+                soma += numeros.charAt(11 - k) * k;
+            }
+
+            resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+            // validação do segundo dígito
+            if (resultado != digitos.charAt(1)) {
+                return false;
+            }
+
+        return true;
+    }
+}
+
+function validacao() {
+    console.log('iniciando validação')
+    document.getElementById('error').style.display = 'none';
+    document.getElementById('success').style.display = 'none';
+    
+
+    let cpf = document.getElementById('inputCpf').value;
+
+    var resultadoValidacao = validaCPF(cpf);
+    console.log(cpf);
+
+    if (resultadoValidacao) { // vai verificar se o resultado é verdadeiro
+        document.getElementById('success').style.display = 'block';
+    } else {
+        document.getElementById('error').style.display = 'block';
+    }
+}
+
+document.getElementById('error').style.display = 'none';
+document.getElementById('success').style.display = 'none';
+document.getElementById('inputCpf').addEventListener('focusout', validacao);
---
+++ js/envioForm.js
@@ -0,0 +1,81 @@
+const Form = () => {
+
+        let form = {
+            
+            nomeCompleto: document.getElementById('inputNome').value,
+            cpf: document.getElementById('inputCpf').value,
+            dataDia: document.getElementById('dataDia').value,
+            dataMes: document.getElementById('dataMes').value,
+            dataAno: document.getElementById('dataAno').value,
+            estadoCivil: document.getElementById('inputEstadoCivil').value,
+            sexo: document.getElementById('inputSexo').value,
+            cep: document.getElementById('inputCep').value,
+            endereco: document.getElementById('inputEndereco').value,
+            numero: document.getElementById('inputNumero').value,
+            complemento: document.getElementById('inputComplemento').value,
+            bairro: document.getElementById('inputBairro').value,
+            cidade: document.getElementById('inputCidade').value,
+            telefone: document.getElementById('inputTelefone').value,
+            email: document.getElementById('inputEmail').value,
+            profissao: document.getElementById('inputProfissao').value,
+            cargo: document.getElementById('inputCargo').value,
+            veiculo: document.getElementById('inputVeiculo').value,
+            cnh: document.getElementById('inputCNH').value,
+        };
+        
+        console.log(form);
+        return form
+        
+} 
+    
+const createCandidate = async (candidato) => {
+      
+    const user = await fetch('https://jobsnet-server.herokuapp.com/register', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type':'application/json'
+              },
+              body: JSON.stringify(Form())
+          });
+          if (user.status == 200) {
+              console.log(Form())
+              alert('deu certo');
+              window.open('./sucesso.html')
+          } if (user.status == 500) {
+              alert ('já temos seus dados');
+              window.open('./dadosDup.html') 
+          }
+        
+}
+
+function checkForm() {
+    let nomeCompleto = document.getElementById('inputNome').value;
+    let cpf = document.getElementById('inputCpf').value;
+    let dataDia = document.getElementById('dataDia').value;
+    let dataMes = document.getElementById('dataMes').value;
+    let dataAno = document.getElementById('dataAno').value;
+    let estadoCivil = document.getElementById('inputEstadoCivil').value;
+    let sexo = document.getElementById('inputSexo').value;
+    let cep = document.getElementById('inputCep').value;
+    let endereco = document.getElementById('inputEndereco').value;
+    let numero = document.getElementById('inputNumero').value;
+    let complemento = document.getElementById('inputComplemento').value;
+    let bairro = document.getElementById('inputBairro').value;
+    let cidade = document.getElementById('inputCidade').value;
+    let telefone = document.getElementById('inputTelefone').value;
+    let email = document.getElementById('inputEmail').value;
+    let profissao = document.getElementById('inputProfissao').value;
+    let cargo = document.getElementById('inputCargo').value;
+    let veiculo = document.getElementById('inputVeiculo').value;
+    let cnh = document.getElementById('inputCNH').value;
+
+    if (nomeCompleto == '' || cpf == '' || dataDia == '' || dataMes == '' || dataAno == ''
+    || cep == '' || endereco == '' || numero == '' || bairro == '' || cidade == ''
+    || telefone == '' || email == false || profissao == '' || cargo == ''){
+        document.getElementById('errorSubmit').style.display = 'block'; 
+    } else {
+        createCandidate();
+        alert('verificando cadastro...');
+    }
+}
